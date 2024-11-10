2fbed3ff1416cf5c32b0bbcdca4735b30c2e0290
Mohamad Faris <66073691+mohamadfarisjaafar@users.noreply.github.com>
2024-11-10 07:56:41 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleMVCApps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
